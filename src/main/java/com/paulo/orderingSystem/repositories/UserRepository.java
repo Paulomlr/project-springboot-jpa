9a31a5686ebf9c7e0becbd78d786f11d185d3a3d
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-06-18 09:14:52 -0300

    Service layer, component registration

--- src/main/java/com/paulo/orderingSystem/repositories/UserRepository.java
+++ src/main/java/com/paulo/orderingSystem/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.paulo.orderingSystem.repositories;
 import com.paulo.orderingSystem.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@Repository -> não é necessário pois ela está herdando do JpaRepository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
