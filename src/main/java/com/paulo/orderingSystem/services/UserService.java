5f62c667257bff532e12b9cf5fd423e096dc492c
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-06-20 10:59:04 -0300

    Exception handling - delete

--- src/main/java/com/paulo/orderingSystem/services/UserService.java
+++ src/main/java/com/paulo/orderingSystem/services/UserService.java
@@ -2,8 +2,11 @@ package com.paulo.orderingSystem.services;
 
 import com.paulo.orderingSystem.entities.User;
 import com.paulo.orderingSystem.repositories.UserRepository;
+import com.paulo.orderingSystem.services.exceptions.DatabaseException;
 import com.paulo.orderingSystem.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
---
+++ src/main/java/com/paulo/orderingSystem/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.paulo.orderingSystem.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
