123dd32400c0395f4f3a343864372e5fa34647fb
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-06-20 11:02:40 -0300

    Exception handling - update

--- src/main/java/com/paulo/orderingSystem/services/UserService.java
+++ src/main/java/com/paulo/orderingSystem/services/UserService.java
@@ -4,6 +4,7 @@ import com.paulo.orderingSystem.entities.User;
 import com.paulo.orderingSystem.repositories.UserRepository;
 import com.paulo.orderingSystem.services.exceptions.DatabaseException;
 import com.paulo.orderingSystem.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
@@ -51,5 +56,4 @@ public class UserService {
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
     }
-
 }
